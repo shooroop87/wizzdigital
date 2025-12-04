08f7fa5a8a58e30f616b87c1161e903384deed68
Andrey <shooroop87@mail.ru>
2025-12-04 10:08:15 +0300

    v2025-12-04 v4
    v2025-12-04 v4

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,31 +1,13 @@
-from core.sitemaps import StaticPagesSitemap
 from django.conf import settings
-from django.conf.urls import handler404
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 
-handler404 = 'api.views.page_not_found'
-handler500 = 'api.views.internal_server_error'
-
-sitemaps = {
-    'static': StaticPagesSitemap,
-}
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.internal_server_error'
 
 urlpatterns = [
-    path('dj-admin/', admin.site.urls),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('', include('core.urls')),
 ]
 
-# Основные URL-паттерны вашего приложения
-urlpatterns += i18n_patterns(
-    path('', include(('api.urls', 'api'), namespace='api')),
-    prefix_default_language=False,   # ← вот это ключ
-)
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/core/context_processors.py
+++
@@ -1,6 +0,0 @@
-# context_processors.py
-from django.conf import settings
-
-def google_maps_api_key(request):
-    print(f"Google Maps API Key: {settings.GOOGLE_MAPS_API_KEY}")  # Для отладки
-    return {'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY}
